ef26630bce98603d5cbf99bb670900db71f763f6
Viserya11 <szucspann@gmail.com>
2022-10-14 16:28:20 +0200

    trying different methods

---
+++ script.js
@@ -0,0 +1,50 @@
+function check() {
+  let c = 0;
+  let q1 = document.quizblock.first.value;
+  if (q1 == "Central Processing Unit") {
+    c++;
+  }
+  document.write(c);
+}
+
+let q2 = document.quizblock.first.value;
+if (q2 == "Central Processing Unit") {
+  c++;
+}
+document.write(c);
+
+let q3 = document.quizblock.first.value;
+if (q3 == "Central Processing Unit") {
+  c++;
+}
+document.write(c);
+
+let q4 = document.quizblock.first.value;
+if (q4 == "Central Processing Unit") {
+  c++;
+}
+document.write(c);
+
+let q5 = document.quizblock.first.value;
+if (q5 == "Central Processing Unit") {
+  c++;
+}
+document.write(c);
+
+let q6 = document.quizblock.first.value;
+if (q6 == "Central Processing Unit") {
+  c++;
+}
+document.write(c);
+
+let q7 = document.quizblock.first.value;
+if (q7 == "Central Processing Unit") {
+  c++;
+}
+document.write(c);
+
+let q8 = document.quizblock.first.value;
+if (q8 == "Central Processing Unit") {
+  c++;
+}
+document.write(c);
