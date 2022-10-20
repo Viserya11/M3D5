172f07ffe6e4bbe1f723f456cf04989056a52e4c
Viserya11 <szucspann@gmail.com>
2022-10-20 13:17:37 +0200

    .

--- script.js
+++ script.js
@@ -4,47 +4,46 @@ function check() {
   if (q1 == "Central Processing Unit") {
     c++;
   }
+
+  let q2 = document.quizblock.first.value;
+  if (q2 == "Central Processing Unit") {
+    c++;
+  }
+  document.write(c);
+
+  let q3 = document.quizblock.first.value;
+  if (q3 == "Central Processing Unit") {
+    c++;
+  }
+  document.write(c);
+
+  let q4 = document.quizblock.first.value;
+  if (q4 == "Central Processing Unit") {
+    c++;
+  }
+  document.write(c);
+
+  let q5 = document.quizblock.first.value;
+  if (q5 == "Central Processing Unit") {
+    c++;
+  }
+  document.write(c);
+
+  let q6 = document.quizblock.first.value;
+  if (q6 == "Central Processing Unit") {
+    c++;
+  }
+  document.write(c);
+
+  let q7 = document.quizblock.first.value;
+  if (q7 == "Central Processing Unit") {
+    c++;
+  }
+  document.write(c);
+
+  let q8 = document.quizblock.first.value;
+  if (q == "Central Processing Unit") {
+    c++;
+  }
   document.write(c);
 }
-
-let q2 = document.quizblock.first.value;
-if (q2 == "Central Processing Unit") {
-  c++;
-}
-document.write(c);
-
-let q3 = document.quizblock.first.value;
-if (q3 == "Central Processing Unit") {
-  c++;
-}
-document.write(c);
-
-let q4 = document.quizblock.first.value;
-if (q4 == "Central Processing Unit") {
-  c++;
-}
-document.write(c);
-
-let q5 = document.quizblock.first.value;
-if (q5 == "Central Processing Unit") {
-  c++;
-}
-document.write(c);
-
-let q6 = document.quizblock.first.value;
-if (q6 == "Central Processing Unit") {
-  c++;
-}
-document.write(c);
-
-let q7 = document.quizblock.first.value;
-if (q7 == "Central Processing Unit") {
-  c++;
-}
-document.write(c);
-
-let q8 = document.quizblock.first.value;
-if (q8 == "Central Processing Unit") {
-  c++;
-}
-document.write(c);
